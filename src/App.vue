ed4fecfdcc10cbf1905638d4eb5c098b659d9c92
Leandro Ciric <lecirics@gmail.com>
2021-11-21 21:11:16 -0300

    feat: added new component, and changed styles on home

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,101 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-system-bar
+      app
+      height="30"
+      color="#e0e0e0"
+    >
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        v-system-bar
+      </v-col>
+    
+    </v-system-bar>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">Reactify?</v-list-item-title>
+          <v-list-item-subtitle>My first Reactify webpage!</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+
+    <v-app-bar
+      app
+      color="#f5f5f5"
+
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      
+      <v-col
+        class="text-center"
+        cols="11"
+      >
+        <span>First react application? Kidding, my first vue application</span>
+      </v-col>
+        
+      
+    </v-app-bar>
+
+    <v-content
+      color="#fafafa"
+    >
+      <ReactApplication/>
+    </v-content>
+    <v-footer 
+      padless
+      color="#f4f4f4"
+
+      >
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        <strong>v-footer</strong>
+      </v-col>
+    </v-footer>
+    <v-bottom-navigation v-model="value">
+     <h2>BOTTOM NAVIGATION</h2>
+    </v-bottom-navigation>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+import ReactApplication from './components/ReactApplication';
+export default {
+  name: 'App',
+  components: {
+    ReactApplication,
+  },
+  data () {
+    return{
+      drawer: false,
+      items: [
+        { title: 'Teste'},
+        { title: 'AF1'},
+        { title: 'DESENVOLVIMENTO'},
+        { title: 'WEB'},
+        { title: 'LAB'},
+      ],
+      right: null,
+    }
+  },
+};
+</script>
---
+++ src/components/ReactApplication.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/react-icon.png')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+
+      <v-col cols="12">
+        <strong>WHO WINS?</strong>
+        <v-img
+          :src="require('../assets/lets-fight.png')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+
+      <v-col cols="12">
+        <strong>WHO WINS?</strong>
+        <v-img
+          :src="require('../assets/pokemon-fighting.gif')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "ReactApplication",
+
+  data: () => ({
+    ecosystem: [
+      {
+        text: "vuetify-loader",
+        href: "https://github.com/vuetifyjs/vuetify-loader",
+      },
+      {
+        text: "github",
+        href: "https://github.com/vuetifyjs/vuetify",
+      },
+      {
+        text: "awesome-vuetify",
+        href: "https://github.com/vuetifyjs/awesome-vuetify",
+      },
+    ],
+    importantLinks: [
+      {
+        text: "Documentation",
+        href: "https://vuetifyjs.com",
+      },
+      {
+        text: "Chat",
+        href: "https://community.vuetifyjs.com",
+      },
+      {
+        text: "Made with Vuetify",
+        href: "https://madewithvuejs.com/vuetify",
+      },
+      {
+        text: "Twitter",
+        href: "https://twitter.com/vuetifyjs",
+      },
+      {
+        text: "Articles",
+        href: "https://medium.com/vuetify",
+      },
+    ],
+    whatsNext: [
+      {
+        text: "Explore components",
+        href: "https://vuetifyjs.com/components/api-explorer",
+      },
+      {
+        text: "Select a layout",
+        href: "https://vuetifyjs.com/getting-started/pre-made-layouts",
+      },
+      {
+        text: "Frequently Asked Questions",
+        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
+      },
+    ],
+  }),
+};
+</script>
